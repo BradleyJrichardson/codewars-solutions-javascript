ed24034122ad2c1e98269dc8ca773237f1bf4726
BradleyJrichardson <brdl.richardson@gmail.com>
2019-05-22 14:09:48 +1000

    solutions

--- 18_6kyu.js
+++ 18_6kyu.js
@@ -8,3 +8,13 @@
 // likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
 // likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
 // For 4 or more names, the number in and 2 others simply increases.
+
+const likes = names => {
+  if (names === []) {
+    return "no one likes this";
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (name.length === 2) {
+    return `${names[0]} and ${names[1]}  like this`;
+  }
+};
