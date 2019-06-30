a82bb4ec92b4c91deea6771c0c556bd0c5ec079e
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-30 17:34:49 +1000

    .

---
+++ 32_5kyu.js
@@ -0,0 +1,37 @@
+// Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+// N! = 1 * 2 * 3 * ... * N
+
+// Be careful 1000! has 2568 digits...
+
+// For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+// Examples
+// zeros(6) = 1
+// # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+// zeros(12) = 2
+// # 12! = 479001600 --> 2 trailing zeros
+
+// const zeros = n => {
+//   numberZeros = 0;
+//   let array = Array.apply(null, { length: n + 1 }).map(Number.call, Number);
+//   for (var i = 0; i < array.length; i++) {
+//     for (var j = i + 1; j < array.length; j++) {
+//       if (array[i] * array[j] === 10) {
+//         numberZeros++;
+//       }
+//     }
+//   }
+//   return numberZeros;
+// };
+
+const zeros = n => {
+  counter = 0;
+  console.log(Math.floor(n % 5));
+
+  //   Divide the number by 5, to find out how much 5 factors are present, then,
+  // Divide the number by 25 to find out how many times 25 are present in a number as it will add extra 5 to number then,
+  // Divide the number by 125 to find out how many times 125 are present in a number as it will add extra 5 to number and so on.
+};
+console.log(zeros(20));
